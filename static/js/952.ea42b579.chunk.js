efe9d604a04bdf2a4e1378e32ae8d8231630f421
Harry Sanders <harrison.w.sanders@gmail.com>
2024-11-19 15:21:16 -0500

    Deploying updates

---
+++ static/js/952.ea42b579.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchore_pref=self.webpackChunkchore_pref||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.ea42b579.chunk.js.map
